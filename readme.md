dac6f9e0d4447eda03747d7eeb3ef1b9993468cd
walkenzhong <walkenzhong@gmail.com>
2020-04-12 21:50:53 +1000

    finish pq

--- readme.md
+++ readme.md
@@ -9,3 +9,13 @@ part one pq function
 
 part two query  
 (nothing)  
+
+## 20200412
+part one pq function  
+完成pq算法(finish)  
+kmeans的stop机制需要修改，添加提前break循环  
+
+part two query  
+1. input queries的切割(doing)  
+2. queries与codebooks的L1距离计算(doing)
+3. 多维距离集合立方体的生成(thinking)
--- submission.py
+++ submission.py
@@ -18,6 +18,10 @@ def kmeans(data,k,init_centroids,max_iter):
     while max_iter > 0:
         #max_iter用于停止kmeans算法
         max_iter -= 1
+        #初始化一个用于存放每个点对应center的list
+        vector_center_index_list = []
+        for i in range(len(data)):
+            vector_center_index_list.append(-1)
         #用来存储每个中心点包含的vectors，由于中心点在变化，所以每次都需要
         center_cluster = []
         for i in range(len(center_list)):
@@ -30,6 +34,7 @@ def kmeans(data,k,init_centroids,max_iter):
         for i in range(len(data)):
             center_point_index = min_distance_index[i]
             vector = data[i]
+            vector_center_index_list[i] = center_point_index
             center_cluster[center_point_index].append(vector)
         #重新计算中心点
         #1,清空原有中心点
@@ -37,9 +42,9 @@ def kmeans(data,k,init_centroids,max_iter):
         #2,按照平均值，重新计算中心点
         for i in center_cluster:
             #print(i)
-            new_center = np.mean(i,axis=0)
+            new_center = np.median(i,axis=0)
             center_list.append(new_center)
-    return center_list
+    return center_list,vector_center_index_list
 #把data的dimensionality划分为P份，返回为切割后的list set
 def split_data(data,P):
     dimensionality = len(data[0])
@@ -70,16 +75,30 @@ def pq(data, P, init_centroids, max_iter):
     #print(data_after_split)
     center_set = []
     k = len(init_centroids)
-    for i in range(len(data_after_split)):
-        tem_center = kmeans(data_after_split[i],k,init_centroids[i],max_iter)
-        center_set.append(tem_center)
-    print(center_set)
-    codebooks = []
     codes = []
+    for i in range(len(data_after_split)):
+        tem_center,tem_codes = kmeans(data_after_split[i],k,init_centroids[i],max_iter)
+        center_set.append(tem_center)
+        codes.append(tem_codes)
+    center_set = np.array(center_set)
+    codes = np.array(codes)
+    codes = np.transpose(codes)
+    '''
+    print(f'p:{P}')
+    print(f'k:{len(init_centroids[0])}')
+    print(f'M:{len(data)}')
+    print(center_set)
+    print(codes)
+    print(np.shape(center_set))
+    print(np.shape(codes))
+    codebooks = []
+    '''
+    codebooks = center_set
     return codebooks, codes
 
 def query(queries, codebooks, codes, T):
-    print(len(queries[0]))
+    print(queries)
+    print(T)
 
     
 if __name__ == '__main__':
